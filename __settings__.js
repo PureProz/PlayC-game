41a0d672c0054b624c2352a39732de014982cf9c
PureProz <131679129+PureProz@users.noreply.github.com>
2024-03-21 18:39:26 -0500

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1971966.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'preferWebGpu': false,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 172090048, 172090051, 172089979, 172090052, 172089988, 172089958, 172089961, 172090026, 172089987, 172090050, 172090049, 172089972, 172089962 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/172090054/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/172090053/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/172090056/1/ammo.js', 'preload' : true},
+];
